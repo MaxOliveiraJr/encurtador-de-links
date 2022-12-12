84f11b5e8ff7b0aadd3d3b6d7453b18728af08d5
Max Oliveira <max-oliveira@outlook.com.br>
2022-12-12 00:22:38 -0300

    feat: create LinkShortener Controller

---
+++ app/Http/Controllers/LinkShortenerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Link;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class LinkShortenerController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'url' => 'required'
+        ]);
+
+        try {
+            $link = new Link();
+            $link->url_from = $request->url;
+            $link->url_to = (string) Str::uuid();
+            $link->validity_until = date('Y/m/d h:m:s', strtotime('+7 days'));
+            $link->save();
+            return $link;
+
+        } catch (\Exception $e) {
+
+            return $e;
+        }
+    }
+}

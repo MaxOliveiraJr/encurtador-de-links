3345b218d8ce348e75ffc93cc64a5e7b3fe81c07
Max Oliveira <max-oliveira@outlook.com.br>
2022-12-12 11:45:46 -0300

    refactor: response catch and date format

--- app/Http/Controllers/LinkShortenerController.php
+++ app/Http/Controllers/LinkShortenerController.php
@@ -5,26 +5,39 @@ namespace App\Http\Controllers;
 use App\Models\Link;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
 
 class LinkShortenerController extends Controller
 {
     public function store(Request $request)
     {
 
-        $request->validate([
-            'url' => 'required'
+
+        $validator = Validator::make($request->all(), [
+
+            'url' => 'required|url'
         ]);
 
+        if ($validator->fails()) {
+
+            return response()->json(["message" => "The url field is required and in URL format"], 422);
+        }
+
+
         try {
-            $link = new Link();
-            $link->url_from = $request->url;
-            $link->url_to = (string) Str::uuid();
-            $link->validity_until = date('Y-m-d h:m:s', strtotime('+7 days'));
-            $link->save();
+
+            $link = Link::create(
+                [
+                    "url_from" => $request->url,
+                    "url_to" => (string) Str::uuid(),
+                    "validity_until" => date('Y-m-d h:m:s.u', strtotime('+7 days'))
+                ]
+            );
+
             return $link;
         } catch (\Exception $e) {
 
-            return $e;
+            return response()->json(["message" => $e], 500);
         }
     }
 }
